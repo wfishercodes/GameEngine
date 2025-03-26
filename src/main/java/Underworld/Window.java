b04eeb0129ec9e53f5efea745bcfb9172b51b0e7
wfishercodes <wfcodes@outlook.com>
2025-03-25 22:49:19 -0400

    Changing of Scenes plus custom Util Package
    Window.java has been updated to be able to change the scene that the user is in. Added in the LevelScene.java, LevelEditorScene.java, Scene.java. Created a custom Util Package

--- src/main/java/Underworld/Window.java
+++ src/main/java/Underworld/Window.java
@@ -3,6 +3,7 @@ package Underworld;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -14,10 +15,11 @@ public class Window {
     private int width, height;
     private String title;
     private long glfwWindow;
-
-    private float r,g,b,a;
+    public float r,g,b,a;
     private boolean fadeToBlack = false;
     private static Window window = null;
+
+    private static Scene currentScene;
 
     private Window(){
         this.width = 1920;
@@ -27,6 +29,21 @@ public class Window {
         g = 1;
         b = 1;
         a = 1;
+    }
+
+    public static void changeScene(int newScene){
+        switch (newScene){
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                //currentScene.init();
+                break;
+            default:
+                assert false : "Unknown scene ' " + newScene + "'";
+        }
     }
 
     public static Window get(){
@@ -96,9 +113,15 @@ public class Window {
         creates the GLCapabilities instance and makes the OpenGL
         bindings available for use.*/
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop(){
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
 
         while(!glfwWindowShouldClose(glfwWindow)){
             // Poll Events
@@ -107,17 +130,16 @@ public class Window {
             glClearColor(r,g,b,a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if(fadeToBlack){
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
-            }
-
-            if(KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
-                fadeToBlack = true;
+            if(dt >= 0) {
+                currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
+
+            // setting the delta time
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
 
     }
