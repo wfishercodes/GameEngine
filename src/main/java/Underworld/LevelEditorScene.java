57d2fb15f2dd1c410c25c6328b1da64a121fe247
wfishercodes <wfcodes@outlook.com>
2025-04-05 22:30:15 -0400

    default.glsl
    default.glsl
    * create noise
    * add time.
    
    LevelEditorScene.java
    * importing the util.Time
    * adding in the abilitiy to use the time.
    
    Shader.java
    * import of matrix4f
    * adds in if the function is being used
    * Uploaded multiple vectors and one Matrix

--- src/main/java/Underworld/LevelEditorScene.java
+++ src/main/java/Underworld/LevelEditorScene.java
@@ -3,6 +3,7 @@ package Underworld;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -108,6 +109,7 @@ public class LevelEditorScene extends Scene {
         defaultShader.use();
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
 
         //bind the VAO
         glBindVertexArray(vaoID);
