c149b7464ae9f20710c015db7d1b7abfc14e4be1
wfishercodes <wfcodes@outlook.com>
2025-04-16 22:53:45 -0400

    RenderBatch
    Added Files
    RenderBatch.java
    * Takes all the rendering from the LevelEditorScene.java and places it here
    
    Renderer.java
    * renders the information from the RenderBatch.java
    
    Transform.java
    * set the transform of the postion and scale
    
    updates to multiple files to work in conjunction with the new systems.

--- src/main/java/Underworld/LevelEditorScene.java
+++ src/main/java/Underworld/LevelEditorScene.java
@@ -1,48 +1,10 @@
 package Underworld;
 
-import components.FontRenderer;
 import components.SpriteRenderer;
-import kotlin.random.Random;
 import org.joml.Vector2f;
-import org.lwjgl.BufferUtils;
-import renderer.Shader;
-import renderer.Texture;
-import util.Time;
-
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-
-import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL20C.glCreateShader;
-import static org.lwjgl.opengl.GL30C.glBindVertexArray;
+import org.joml.Vector4f;
 
 public class LevelEditorScene extends Scene {
-
-    private int vertexID, fragmentID, shaderProgram;
-
-    //sets the color for each vertex
-    private float[] vertexArray = {
-            //position          //color
-            100f, 0f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f,      1,1,//bottom right  0
-            0f, 100f, 0.0f,     0.0f, 1.0f, 0.0f, 1.0f,      0,0,//top left      1
-            100f, 100f, 0.0f,   1.0f, 0.0f, 1.0f, 1.0f,      1,0,//top right     2
-            0f, 0f, 0.0f,       1.0f, 1.0f, 0.0f, 1.0f,      0,1//bottom left    3
-    };
-    //IMPORTANT: Must be in counter-clockwise order
-    //Sets the location for each triangle
-    private int[] elementArray = {
-            2,1,0, //top right triangle
-            0,1,3, //bottom left triangle
-    };
-
-    private int vaoID, vboID, eboID;
-    private  Shader defaultShader;
-    private Texture testTexture;
-
-    GameObject testObj;
-    private boolean firstTime = false;
-
 
     public LevelEditorScene(){
 
@@ -50,97 +12,38 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
-        System.out.println("Creating 'test object");
-        this.testObj = new GameObject("test object");
-        this.testObj.addComponent(new SpriteRenderer());
-        this.testObj.addComponent(new FontRenderer());
-        this.addGameObjectToScene(this.testObj);
+        this.camera = new Camera(new Vector2f());
 
-        this.camera = new Camera(new Vector2f(-200, -300));
-        defaultShader = new Shader("assets/shaders/default.glsl");
-        defaultShader.compile();
-        this.testTexture = new Texture("assets/images/testImage.jpg");
+        int xOffset = 10;
+        int yOffset = 10;
 
-        //Generating the VAO, VBO, and EBO buffer objects, and send them to the GPU
-        vaoID = glGenVertexArrays();
-        glBindVertexArray(vaoID);
+        float totalWidth = (float)(600 - xOffset * 2);
+        float totalHight = (float)(300 - yOffset * 2);
+        float sizeX = totalWidth / 100.0f;
+        float sizeY = totalHight / 100.0f;
 
-        //create a float buffer of vertices
-        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
-        vertexBuffer.put(vertexArray).flip();
+        for (int x=0; x < 100; x++){
+            for (int y=0; y < 100; y++){
+                float xPos = xOffset + (x * sizeX);
+                float yPos = yOffset + (y * sizeY);
 
-        //create VBO upload the vertex buffer
-        vboID = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboID);
-        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
-
-        //create the indices and upload
-        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
-        elementBuffer.put(elementArray).flip();
-
-        eboID = glGenBuffers();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
-
-        //add the vertex attribute pointers
-        int positionsSize = 3;
-        int colorSize = 4;
-        int uvSize = 2;
-        int vertexSizeBytes = (positionsSize + colorSize) * Float.BYTES;
-
-        glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
-        glEnableVertexAttribArray(1);
-
-        glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize + colorSize) * Float.BYTES);
-        glEnableVertexAttribArray(2);
+                GameObject go = new GameObject("Obj" + x + "" + y, new Transform(new Vector2f(xPos, yPos), new Vector2f(sizeX,
+                        sizeY)));
+                go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth, yPos / totalHight, 1, 1)));
+                this.addGameObjectToScene(go);
+            }
+        }
     }
 
     @Override
     public void update(float dt) {
-        camera.position.x -= dt * 50f;
-
-        defaultShader.use();
-
-        //Upload Texture to shader
-        defaultShader.uploadTexture("TEX_SAMPLER", 0);
-        glActiveTexture(GL_TEXTURE0);
-        testTexture.bind();
-
-        defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
-        defaultShader.uploadMat4f("uView", camera.getViewMatrix());
-        defaultShader.uploadFloat("uTime", Time.getTime());
-
-        //bind the VAO
-        glBindVertexArray(vaoID);
-
-        //enable the vertex attribute pointers
-        glEnableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-
-        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
-
-        //unbind everything
-        glDisableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-
-        glBindVertexArray(0);
-
-        defaultShader.detach();
-
-        if(!firstTime) {
-            System.out.println("Creating Game object");
-            GameObject go = new GameObject("Game Test 2");
-            go.addComponent(new SpriteRenderer());
-            this.addGameObjectToScene(go);
-            firstTime = true;
-        }
+        System.out.println("FPS: " + (1.0f /dt));
 
         for (GameObject go : this.gameObjects){
             go.update(dt);
         }
+
+        this.renderer.render();
     }
 
 }
